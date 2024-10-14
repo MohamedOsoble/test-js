63d0c3ad2d1de6a86c97c02abd095fbc98277ae4
MohamedOsoble <MohamedOsoble@outlook.com>
2024-10-14 14:59:22 +0100

    initialize files for first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+function sum(a, b) {
+    return a + b;
+  }
+
+
+function capitalize(){
+    return
+};
+
+function reverseString(){
+    return;
+};
+
+class Calculator{
+    sum(){
+        return;
+    }
+    subtract(){
+        return;
+    }
+    divide(){
+        return;
+    };
+    multiply(){
+        return;
+    };
+};
+
+function caesarCipher(){
+    return;
+};
+
+function analyzeArray(){
+    return;
+};
+
+export {sum, capitalize, reverseString, Calculator, caesarCipher, analyzeArray};
