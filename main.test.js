79f8d3dffaccdd9e954d46c0a773872a94e47acb
MohamedOsoble <Mohamed.Osoble@outlook.com>
2024-10-14 17:44:55 +0100

    feat: add Calculator.sum() tests

--- main.test.js
+++ main.test.js
@@ -1,8 +1,5 @@
 import {sum, capitalize, reverseString, Calculator, caesarCipher, analyzeArray} from './main';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
 
 test('capitalizes the first letter of the input string', () => {
   expect(capitalize('hello')).toMatch('Hello');
@@ -14,4 +11,10 @@ test('reverses the input string', () => {
   expect(reverseString('Hello World')).toMatch('dlroW olleH');
   expect(reverseString('123normandy')).toMatch('ydnamron321');
   expect(reverseString('j')).toMatch('j');
+});
+
+test('adds x + y to equal z', () => {
+  expect(Calculator.sum(1, 2)).toBe(3);
+  expect(Calculator.sum(5, 25)).toBe(30);
+  expect(Calculator.sum(-25, -5)).toBe(-30);
 });
