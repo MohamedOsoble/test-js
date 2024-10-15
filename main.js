fb1396e180906d0aa15ddfbe97dc4f4989e31669
MohamedOsoble <Mohamed.Osoble@outlook.com>
2024-10-15 18:35:36 +0100

    feat: complete test and functionaility for caesarCipher

--- main.js
+++ main.js
@@ -19,15 +19,53 @@ const calculator = (function () {
     return {add, subtract, multiply, divide};
 })();
 
-function caesarCipher(){
-    return;
+function encrypt(char, start, key){
+    let encryptedCharCode = 0;
+    if((char + key) > (start + 25)){
+        let newKey = (char + key) - (start + 26)
+        encryptedCharCode = start + newKey;
+    }
+    else{
+        encryptedCharCode = char + key;
+    };
+    return encryptedCharCode;
+};
+
+function caesarCipher(message, key){
+
+    // Initialize Encrypted Message
+    let encryptedMessage = "";
+
+    for(let i = 0; i < message.length; i++){
+        // Initialize current Character & Encrypted Char
+        let charCode = message.charCodeAt(i);
+        let encryptedCharCode = 0;
+
+        // If lowercase, encrypt lower
+        if(charCode >= 65 && charCode <= 90){
+            encryptedCharCode = encrypt(charCode, 65, key)
+        }
+
+        // If uppercase, encrypt upper
+        else if(charCode >= 97 && charCode <= 122){
+            encryptedCharCode = encrypt(charCode, 97, key);
+        }
+
+        // else, just add char
+        else{
+            encryptedCharCode = charCode;
+        };
+
+        encryptedMessage += String.fromCharCode(encryptedCharCode);
+    };
+    return encryptedMessage;
 };
 
 function calcAvg(arr){
     let length = arr.length;
     let sum = 0;
     arr.forEach((element) => sum += element)
-        
+
     return sum / length;
 }
 
@@ -39,7 +77,7 @@ function analyzeArray(inputArr){
             max: 0,
             length: 0
         };
-    }
+    };
     return {
         average: calcAvg(inputArr),
         min: Math.min(...inputArr),
